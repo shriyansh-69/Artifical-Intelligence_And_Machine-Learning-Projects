41fbcb63a27bd2adb42689fa397440c574b4ec09
shriyansh-69 <shriyansh457tiwari@gmail.com>
2026-01-22 10:56:45 +0530

    "Loading The Json File To Ready"

--- AI-Customer-Support-Chatbot/main.py
+++ AI-Customer-Support-Chatbot/main.py
@@ -7,4 +7,9 @@ import nltk
 from nltk.stem import WordNetLemmatizer
 import tensorflow as tf
 
+lemmatizer = WordNetLemmatizer()
 
+path = r"D:\Machine_Learning_Projects\AI-Customer-Support-Chatbot\intent.json"
+
+with open(path, "r", encoding= "utf-8") as f:
+    intent = json.loads(f.read())
